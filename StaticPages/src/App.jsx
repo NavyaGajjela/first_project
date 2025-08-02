9d6f7e246f53958a42f04c82ca00d9028f7507b8
Navya <gajjelanavya2@gmail.com>
2025-08-02 16:12:27 +0530

    commit all changes

--- StaticPages/src/App.jsx
+++ StaticPages/src/App.jsx
@@ -1,9 +1,28 @@
+import logo from './assets/react_logo.png';
 
+
+
+ function  Header(){
+ return(
+    <header className="header">
+    <img src = {logo} className="logo" alt="React logo"/>
+        <nav>
+            <ul className="nav-items">  
+                <li className="nav-list-items">Pricing</li>
+                <li className="nav-list-items">About</li>
+                <li className="nav-list-items">Contact</li>
+            </ul>
+        </nav>
+     </header>
+ );
+
+}
 //App.jsx
-
 function App(){
     return(
     <>
+
+    <Header />
     <main>
     <h1> Reason i am excited to learn React</h1>
     <ol>
@@ -11,6 +30,7 @@ function App(){
         <li>Having a skill with react help in get jobs in frontend role</li>
     </ol>
     </main>
+
         <footer>
             <small>Navya.All rights are reserved</small>
         </footer>
